00c9cd218a73beca397579ab2ba1b98b2d22ee56
Sagar <sagarsandy492@gmail.com>
2019-01-02 09:00:27 +0530

    Base Commit

---
+++ LinkedIn Integration/LinkedIn Integration/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LinkedIn Integration
+//
+//  Created by Sagar Sandy on 02/01/19.
+//  Copyright © 2019 Sagar Sandy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
